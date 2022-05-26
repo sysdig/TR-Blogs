c2db1827fd5f58fdcfbb538fa9fd74d49c45ce2b
javery <jason.avery@sysdig.com>
2022-05-26 09:47:14 -0500

    Atomic Red Docker

---
+++ atomic-red-docker/atomic-parse.py
@@ -0,0 +1,57 @@
+#!/bin/env python3
+import os
+import re
+import sys
+import yaml
+
+def usage():
+	print('%s: <AtomicRedTeam/atomics path>' % sys.argv[0])
+	sys.exit(1)
+	return
+
+def main():
+	data=[]
+
+	try:
+		# path to AtomicRedTeam/atomics directory
+		dirpath = sys.argv[1]
+		for root, dirs, files in os.walk(dirpath, topdown=False):
+			for name in files:
+				if name.endswith('.yaml'):
+					yamlfile = os.path.join(root, name)
+					f = open(yamlfile, 'r')
+					doc = yaml.load(f.read(), Loader=yaml.Loader)
+					f.close()
+					if 'attack_technique' not in doc:
+						continue
+					newdata = {'id': doc['attack_technique'], 'name': doc['display_name'], 'tests': []}
+					for section in doc['atomic_tests']:
+						sectiondata = {'name': section['name'], 'desc': re.sub(r'[\t\r\n]', ' ', section['description']), 'platforms': section['supported_platforms']}
+						if 'executor' in section:
+							if 'command' in section['executor']:
+								# collapse multi-command steps to one line
+								sectiondata['cmd'] = re.sub(r'[\t\r\n]', ' ; ', section['executor']['command'])
+						newdata['tests'].append(sectiondata)
+					data.append(newdata)
+	except:
+		usage()
+
+	# Now, write out a tab delimited file.
+	outfile = 'atomic-parsed.tsv'
+	f = open(outfile, 'w')
+	f.write('id\tid name\ttest name\ttest desc\tplatform\tcommand\n')
+
+	for attk_item in data:
+		for test_item in attk_item['tests']:
+			for platform in test_item['platforms']:
+				outstring = '%s\t%s\t%s\t%s\t%s\t' % (attk_item['id'], attk_item['name'], test_item['name'], test_item['desc'], platform)
+				if 'cmd' in test_item:
+					outstring += test_item['cmd']
+				outstring += '\n'
+				f.write(outstring)
+	f.close()
+
+	print('%s created.' % outfile)
+	return
+
+if __name__ == '__main__': main()
